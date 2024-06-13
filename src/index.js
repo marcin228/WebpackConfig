4e11c9d7b3f5005800137f0d82cbfa4f66462161
Marcin228 <marcin@goingforit.pl>
2024-06-13 23:20:07 +0200

    Add git ignore.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Animal from './modules/Animal.js';
+import Food from './modules/Food.js';
+import Zoo from './modules/Zoo.js';
+
+const animal = new Animal();
---
+++ src/modules/Zoo.js
@@ -0,0 +1,5 @@
+export default class Zoo{
+
+    constructor(){
+    }
+}
