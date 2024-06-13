e5d9bb1165eb89389798cd9b52ae48511210093f
Marcin228 <marcin@goingforit.pl>
2024-06-13 23:51:39 +0200

    Add polyfills.

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: [
+        [
+            '@babel/preset-env',
+            {
+                useBuiltIns: 'usage',
+                corejs: '3.0'
+            }
+        ]
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,14 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
     }
 };
