ca8ac0a7d9ed2b384075d9f24f12be2f70fd3f6f
Marcin228 <marcin@goingforit.pl>
2024-06-15 23:35:59 +0200

    Add brwoserlist.

--- src/modules/Animal.js
+++ src/modules/Animal.js
@@ -1,12 +1,32 @@
-export default class Animal{
+export default class Animal extends HTMLElement{
 
     constructor(name){
 
-        this._name = name; 
+        super();
+
+        this._name = name;
+        this._hunger = 0;
+
+        this.innerText = name;
+    }
+
+    eat(food){
+
+        //this.hunger += food.calories;
     }
 
     get name(){
 
         return this._name;
+    }
+
+    get hunger(){
+
+        return this._hunger;
+    }
+
+    update(){
+    
+        this.innerHTML = this._name + ' / ' + this._hunger;
     }
 }
