75f888a0272ba8dc8aeaf2f86cb3c33754181465
Marcin228 <marcin@goingforit.pl>
2024-06-20 23:58:33 +0200

    Add TS loader.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@ import Food from './modules/Food.js';
 import Zoo from './modules/Zoo.js';
 
 const animal = new Animal();
+
+const body = document.querySelectorAll('body');
+const animalDOM = new 
+
+document.body.appendChild(animal);
